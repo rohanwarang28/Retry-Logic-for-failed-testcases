5dcb5d30cac34b7c808815c1fc5963d6f8d4950a
unknown <rohanwarang28@yahoo.com>
2018-01-20 22:13:05 +0530

    added logic to add screenshots along with retry logic

---
+++ src/main/java/Utility/TestUtil.java
@@ -0,0 +1,20 @@
+package Utility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class TestUtil {
+
+	
+	public static void takesScreenShotAtEnd(WebDriver driver) throws IOException {
+	TakesScreenshot ts= (TakesScreenshot) driver;
+	File source = ts.getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(source,new File("./errorScreenshots/"+System.currentTimeMillis()+".png"));
+	}
+
+}
--- src/test/java/mytests/Tests.java
+++ src/test/java/mytests/Tests.java
@@ -3,21 +3,51 @@ package mytests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Tests {
+import Utility.EventListener;
 
+
+public class Tests {
+	 
+	WebDriver driver;
+	public EventListener eventListener;
+	public  EventFiringWebDriver e_driver;
+	//public static WebEventListener eventListener;
+
+	@BeforeMethod
+	public void setup(){
+		
+		
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		e_driver=new EventFiringWebDriver(driver);
+		eventListener=new EventListener();
+		e_driver.register(eventListener);
+		driver=e_driver;
+	}
+   	 
+
+  
 	@Test
 	public void Amazon() 
 	{
-		System.out.println("amazon working fine");
-		Assert.assertFalse(true);
+		driver.get("https://www.amazon.com");
+		boolean flag=driver.findElement(By.id("1253")).isDisplayed();
+		//Assert.assertFalse(flag);
+		
 	}
 	
 	@Test
 	public void Wordpress() 
-	{
+	{   driver.get("https://www.wordpress.com");
 		System.out.println("Wordpress working fine");
 		
 	}
